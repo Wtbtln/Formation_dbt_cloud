40bf2ebf8216ccd6e09065d82e59f8d70d8298c0
Wentao Bastard <wentao.bastard@talan.com>
2025-01-16 00:32:00 +0000

    modifie stg_stripe__payment.sql

--- models/stging/stripe/stg_stripe__payment.sql
+++ models/stging/stripe/stg_stripe__payment.sql
@@ -1,5 +1,8 @@
 select
  id as payment_id,
- orderid,
- amount
+ orderid as order_id,
+ paymentmethod as payment_method,
+ status,
+ amount/100 as amount,
+ created as created_at
 from {{ source('stripe', 'payment') }}
